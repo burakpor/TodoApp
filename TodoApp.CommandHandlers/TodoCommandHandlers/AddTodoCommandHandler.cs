94ccb41db1e66fc9db888b98480818da46b104b9
Burak Portakal <burak.portakal@outlook.com>
2021-03-28 13:25:44 +0300

    add todo command added.

---
+++ TodoApp.CommandHandlers/TodoCommandHandlers/AddTodoCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using TodoApp.Commands.TodoCommands;
+using TodoApp.Models.BusinessModels;
+
+namespace TodoApp.CommandHandlers.TodoCommandHandlers
+{
+    public class AddTodoCommandHandler : CommandHandler<AddTodoCommand, UserLoginResponse>
+    {
+        protected override Task<UserLoginResponse> ProcessCommand(AddTodoCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TodoApp.Models/Dtos/AddTodoDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models.Dtos
+{
+    public class AddTodoDto
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
